28b314882fa269ed8e0ffa023a14935eab5ec95b
lorenashfield <lashfield@ucsb.edu>
2024-09-30 16:42:33 -0700

    adding fields

--- technique_fields.py
+++ technique_fields.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass, field
 
 @dataclass
+class OCV:
+    rest_time: float = 0.0
+    record_de: float = 0.0
+    record_dt: float = 0.0
+
+@dataclass
 class CV:
     vs_init: list[bool] = field(default_factory=lambda: [False] * 5)
     v_step: list[float] = field(default_factory=lambda: [0.0] * 5)
@@ -11,6 +17,16 @@ class CV:
     n_cycles: int = 0
     begin_i: float = 0.0
     end_i: float = 0.0
+
+@dataclass
+class CP:
+    i_step: list[float] = field(default_factory=lambda: [0.0] * 100)
+    vs_init: list[bool] = field(default_factory=lambda: [False] * 100)
+    duration_step: list[float] = field(default_factory=lambda: [0.0] * 100)
+    step_number: int = 0
+    record_dt: float = 0.0
+    average_di: float = 0.0
+    n_cycles: int = 0
 
 @dataclass
 class CA:
